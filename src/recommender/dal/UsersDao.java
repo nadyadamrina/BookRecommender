a48b19f42b41565569dc9776d0669179cbf15b69
Nadiia Ramthun <ramthun.n@northeastern.edu>
2021-11-15 15:48:18 -0800

    CRUD operations for Users

--- src/recommender/dal/UsersDao.java
+++ src/recommender/dal/UsersDao.java
@@ -1,5 +1,12 @@
 package recommender.dal;
 
+import recommender.model.Users;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsersDao {
     protected ConnectionManager connectionManager;
 
@@ -12,5 +19,126 @@ public class UsersDao {
             instance = new UsersDao();
         }
         return instance;
+    }
+
+    public Users create(Users user)  throws SQLException {
+        String insertUser = "INSERT INTO Users(UserName,Password,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?,?);";
+        Connection connection = null;
+        PreparedStatement insertStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            insertStmt = connection.prepareStatement(insertUser);
+
+            insertStmt.setString(1, user.getUserName());
+            insertStmt.setString(2, user.getPassword());
+            insertStmt.setString(3, user.getFirstName());
+            insertStmt.setString(4, user.getLastName());
+            insertStmt.setString(5, user.getPhone());
+            insertStmt.setString(6, user.getEmail());
+
+            insertStmt.executeUpdate();
+
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if(connection != null) {
+                connection.close();
+            }
+            if(insertStmt != null) {
+                insertStmt.close();
+            }
+        }
+    }
+
+    public Users getUserFromUserName(String userName) throws SQLException {
+        String selectUser = "SELECT UserName,Password,FirstName,LastName,Phone,Email FROM Users WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement selectStmt = null;
+        ResultSet results = null;
+        try {
+            connection = connectionManager.getConnection();
+            selectStmt = connection.prepareStatement(selectUser);
+            selectStmt.setString(1, userName);
+
+            results = selectStmt.executeQuery();
+
+            if(results.next()) {
+                String resultUserName = results.getString("UserName");
+                String password = results.getString("Password");
+                String firstName = results.getString("FirstName");
+                String lastName = results.getString("LastName");
+                String phone = results.getString("Phone");
+                String email = results.getString("Email");
+
+                return new Users(resultUserName, password, firstName, lastName, phone, email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if(connection != null) {
+                connection.close();
+            }
+            if(selectStmt != null) {
+                selectStmt.close();
+            }
+            if(results != null) {
+                results.close();
+            }
+        }
+        return null;
+    }
+
+    public Users updateName(Users user, String newFirstName, String newLastName) throws SQLException {
+        String updateUser = "UPDATE Users SET FirstName=?, LastName=? WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement updateStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            updateStmt = connection.prepareStatement(updateUser);
+            updateStmt.setString(1, newFirstName);
+            updateStmt.setString(2, newLastName);
+            updateStmt.setString(3, user.getUserName());
+            updateStmt.executeUpdate();
+
+            user.setLastName(newLastName);
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if(connection != null) {
+                connection.close();
+            }
+            if(updateStmt != null) {
+                updateStmt.close();
+            }
+        }
+    }
+
+    public Users delete(Users user) throws SQLException {
+        String deleteUser = "DELETE FROM Users WHERE UserName=?;";
+        Connection connection = null;
+        PreparedStatement deleteStmt = null;
+        try {
+            connection = connectionManager.getConnection();
+            deleteStmt = connection.prepareStatement(deleteUser);
+            deleteStmt.setString(1, user.getUserName());
+            deleteStmt.executeUpdate();
+
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if(connection != null) {
+                connection.close();
+            }
+            if(deleteStmt != null) {
+                deleteStmt.close();
+            }
+        }
     }
 }
